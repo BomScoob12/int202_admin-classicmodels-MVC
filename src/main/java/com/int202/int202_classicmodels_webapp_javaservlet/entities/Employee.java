428f80cb6200671e1813b48f0296c5b4867c13bd
BomScoob <sorawitkraukham@gmail.com>
2023-12-16 16:31:44 +0700

    Employee password update and argon hash

--- src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Employee.java
+++ src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Employee.java
@@ -13,19 +13,25 @@ import lombok.ToString;
 @Entity
 @Table(name = "employees")
 @NamedQueries({
-        @NamedQuery(name = "EMP.FIND_ALL", query = "SELECT emp FROM Employee emp")
+        @NamedQuery(name = "EMP.FIND_ALL", query = "SELECT emp FROM Employee emp"),
+        @NamedQuery(name = "EMP.FIND_BY_EMAIL", query = "SELECT emp FROM Employee emp WHERE emp.email = :emp_email")
 })
 public class Employee {
     @Id
     @Column(name = "employeeNumber", nullable = false)
-    private int id;
+    private Integer id;
     @Column(name = "lastName")
     private String lastname;
     @Column(name = "firstName")
     private String firstname;
     private String extension;
     private String email;
+    private String password;
     private String officeCode;
     private Integer reportsTo;
     private String jobTitle;
+
+    @ManyToOne
+    @JoinColumn(name = "officeCode", insertable = false, updatable = false)
+    private Office office;
 }
--- src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Office.java
+++ src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Office.java
@@ -1,4 +1,34 @@
 package com.int202.int202_classicmodels_webapp_javaservlet.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "offices")
+
+@NamedQueries({
+        @NamedQuery(name = "OFFICE.FIND_ALL", query = "SELECT o from Office o")
+})
 public class Office {
+    @Id
+    private String officeCode;
+    private String city;
+    private String phone;
+    private String addressLine1;
+    private String addressLine2;
+    private String state;
+    private String country;
+    private String postalCode;
+    private String territory;
+
+    @OneToMany(mappedBy = "officeCode")
+    private List<Employee> employeeList;
 }
