cc3cfe20fba04e10966ad37a0252381eb8d97510
BomScoob <sorawitkraukham@gmail.com>
2023-12-16 14:34:31 +0700

    Employee entity

---
+++ src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/controller/OfficeListServlet.java
@@ -0,0 +1,23 @@
+package com.int202.int202_classicmodels_webapp_javaservlet.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "OfficeListServlet", value = "/office-list")
+public class OfficeListServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
--- src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Employee.java
+++ src/main/java/com/int202/int202_classicmodels_webapp_javaservlet/entities/Employee.java
@@ -1,4 +1,30 @@
 package com.int202.int202_classicmodels_webapp_javaservlet.entities;
 
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+@Entity
+@Table(name = "employees")
+@NamedQueries({
+        @NamedQuery(name = "FIND_ALL", query = "SELECT emp FROM Employee emp")
+})
 public class Employee {
+    @Id
+    private int id;
+    @Column(name = "lastName")
+    private String lastname;
+    @Column(name = "firstName")
+    private String firstname;
+    private String extension;
+    private String email;
+    private String officeCode;
+    private int reportsTo;
+    private String jobTitle;
 }
